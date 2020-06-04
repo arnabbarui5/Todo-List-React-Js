55db5497932cc04201cf3af482220648a47793c4
Arnab Barui <40000635+arnabbarui5@users.noreply.github.com>
2020-06-05 00:22:56 +0530

    todo list react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import ToDoList from './ToDoList';
 
-function App() {
+const App = () => {
+
+  const [add, setAdd] = useState("");
+  const [list, addL] = useState([]);
+
+  const itemEvents = (event) => {
+    setAdd(event.target.value);
+  }
+
+  const addList = () => {
+    addL((oldList) => {
+      return [...oldList, add];
+    });
+    setAdd("");
+  };
+
+  const deleteItems = (id) => {
+    console.log('deleted');
+
+    addL((oldList) => {
+      return oldList.filter((arrElements, index) => {
+        return index !== id; 
+      })
+    })
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <React.Fragment>
+      <div className="main-div">
+        <div className="center-div">
+          <br />
+          <h1>TODO List</h1>
+          <br />
+          <input type="text" placeholder="Add a name" onChange={itemEvents} value={add}/>
+          <button onClick={addList}> + </button>
+          <ol>
+              {list.map((itemVal, index) => {
+                return <ToDoList
+                key={index} 
+                id={index} 
+                text={itemVal}
+                onSelect={deleteItems}
+                />
+        })}
+        </ol>
+        </div>
+      </div>
+    </React.Fragment>
 
+  )
+}
 export default App;
